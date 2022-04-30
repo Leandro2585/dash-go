ca8b296826885691b36d0cf3ff0e87b535417ae0
leandro2585 <leo.real2585@gmail.com>
2022-04-29 21:54:10 -0300

    feat: add reponsive dynamic menu in side-bar component

--- src/components/header/profile.tsx
+++ src/components/header/profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
 
-export const Profile = () => {
+type Props = {
+  showProfileData?: boolean 
+}
+
+export const Profile = ({ showProfileData }: Props) => {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
-        <Text>Leandro Real</Text>
-        <Text color='gray.300' fontSize='small'>leo.real2585@gmail.com</Text>
-      </Box>
+      { showProfileData && (
+        <Box mr='4' textAlign='right'>
+          <Text>Leandro Real</Text>
+          <Text color='gray.300' fontSize='small'>leo.real2585@gmail.com</Text>
+        </Box>
+      )}
       <Avatar size='md' name='Leandro Real' src='https://github.com/leandro2585.png'/>
     </Flex>
   )
--- src/components/side-bar/index.ts
+++ src/components/side-bar/index.ts
@@ -1,3 +1,4 @@
 export * from './side-bar'
 export * from './nav-link'
 export * from './nav-section'
+export * from './side-bar-nav'
